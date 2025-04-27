c9e9f669f7d36a425d468f998bbadaefe50dcb19
Dineshkumar-r61 <dineshramachandran61@gmail.com>
2025-04-27 19:02:20 +0530

    Add files via upload
    Week1. day2

---
+++ day2/Browser.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class Browser {
+    String launchBrowser(String browserName ){
+	System.out.println("Browser launched sussessfully");
+	return browserName;
+	}
+    
+    void loadURL() {
+    System.out.println("Application url loaded successfully");
+    }
+    
+    public static void main(String[] args) {
+    	Browser browser = new Browser();
+        String RS = browser.launchBrowser("Chrome");
+        browser.loadURL();
+    	System.out.println("browserName:"+" "+ RS);
+	}  
+}
