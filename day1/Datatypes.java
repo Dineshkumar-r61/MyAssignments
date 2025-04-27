1cc873255d58676850f35cfc811c0cc8e678734e
Dineshkumar-r61 <dineshramachandran61@gmail.com>
2025-04-27 19:09:32 +0530

    Add files via upload
    Week 1 - Day 1

---
+++ day1/Datatypes.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		
+		int x=57;
+		System.out.println(x);
+		
+		float deci=90.2348476f;
+		System.out.println(deci);
+		
+		char nameInitial='R';
+		System.out.println(nameInitial);
+		
+		boolean ups=true;
+		System.out.println(ups);
+		
+	    String tool="Selenium";
+	    System.out.println(tool);
+
+	}
+
+}
